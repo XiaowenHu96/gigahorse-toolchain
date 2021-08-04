7cc76c68a20bd5fd98449fb6007331889ef2836c
XiaowenHu96 <xihu5895@uni.sydney.edu.au>
2021-08-04 02:52:43 +0000

    updates pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+generatefacts=/home/xihu5895/gigahorse-toolchain/generatefacts
+souffleaddon=/home/xihu5895/gigahorse-toolchain/souffle-addon
+gen_bytecode=/home/xihu5895/gigahorse-toolchain/clients/gen_bytecode.py
+souffle=/home/xihu5895/gigahorse-toolchain/decompiler_compiled
+
+input=$1
+facts=$2
+out=$3
 
 set -e
 set -x
-./generatefacts $1 facts
-bash -c "echo \"8\" > facts/MaxContextDepth.csv"
-LD_LIBRARY_PATH=/home/xiaowen/gigahorse-toolchain/souffle-addon ./prog  -F facts -D out
-cd out && ../clients/gen_bytecode.py
+$generatefacts $input $facts
+bash -c "echo \"8\" > $facts/MaxContextDepth.csv"
+mkdir -p $out
+if [[ $# -eq 4 ]];
+then
+    LD_LIBRARY_PATH=$souffleaddon timeout $4 $souffle  -F $facts -D $out
+else
+    LD_LIBRARY_PATH=$souffleaddon $souffle  -F $facts -D $out
+fi
+cd $out && $gen_bytecode
